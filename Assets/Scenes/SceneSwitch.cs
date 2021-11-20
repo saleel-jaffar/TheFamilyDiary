38cf15c8885e6b780446e24edcd1c767f8520b9d
Saleel Jaffar <saleel.jaffar@gmail.com>
2021-11-20 22:19:01 +0300

    Final

---
+++ Assets/Scenes/SceneSwitch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour {
+   void OnTriggerEnter(Collider other) {
+       loadNextScene();
+   }
+
+   public void loadNextScene() {
+       int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+       SceneManager.LoadScene(currentSceneIndex + 1);
+   }
+
+   public void loadStartScene() {
+       SceneManager.LoadScene(0);
+   }
+}
